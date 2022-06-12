ad31832db306ca5d0cf5ebaec5613154139a5d25
Vanili <katevanili98@gmail.com>
2022-06-12 20:42:46 +0300

    Added the comments model

--- awards/models.py
+++ awards/models.py
@@ -41,3 +41,8 @@ class Rates(models.Model):
     content=models.PositiveIntegerField(default=0,validators=[MaxValueValidator(10)])
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     project=models.IntegerField(default=0)
+
+class Comments(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    comment=models.TextField(max_length=200)
+    pro_id=models.IntegerField(default=0)
