e23907fe4773022d8321dfb7a00ba9a3d434b7ea
Vanili <katevanili98@gmail.com>
2022-06-12 21:03:21 +0300

    Registered my models

--- awards/admin.py
+++ awards/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile,Projects,Rates,Comments
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Projects)
+admin.site.register(Rates)
+admin.site.register(Comments)
